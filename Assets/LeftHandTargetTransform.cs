857ec6db925644f34781f51d5c2aca60e44b7cd8
willy <willyjen1111@gmail.com>
2024-12-26 15:45:46 +0800

    [29課] 左手ik真歲不同武器設定位置

---
+++ Assets/LeftHandTargetTransform.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class LeftHandTargetTransform : MonoBehaviour
+{
+    //需要這個腳本去偵測遊戲物件
+    //檢測左手目標位置及旋轉
+}
--- Assets/WeaponVisualController.cs
+++ Assets/WeaponVisualController.cs
@@ -13,6 +13,16 @@ public class WeaponVisualController : MonoBehaviour
     [SerializeField] private Transform shotgun;
     [SerializeField] private Transform rifle;
 
+    private Transform currentGun;
+
+    [Header("左手IK")]
+    [SerializeField] private Transform leftHand;
+
+    private void Start()
+    {
+        SwitchOn(pistol);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -45,6 +55,9 @@ public class WeaponVisualController : MonoBehaviour
     {
         SwitchOffGuns();
         gunTransform.gameObject.SetActive(true);
+        currentGun = gunTransform;
+
+        AttachLeftHand();
     }
 
     private void SwitchOffGuns()
@@ -54,4 +67,13 @@ public class WeaponVisualController : MonoBehaviour
             gunTransforms[i].gameObject.SetActive(false);
         }
     }
+
+    private void AttachLeftHand()
+    {
+        Transform targetTransform = currentGun.GetComponentInChildren<LeftHandTargetTransform>().transform;
+        //localPosition:物件相對於其 父物件 的位置,position:物件在 世界座標系 中的絕對位置
+        leftHand.localPosition = targetTransform.localPosition;
+        leftHand.localRotation = targetTransform.localRotation;
+    }
+
 }
