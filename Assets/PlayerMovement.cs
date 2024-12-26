bbbe11ba67fdb2cce8acc5c211d9d3fef4dd11a1
willy <willyjen1111@gmail.com>
2024-12-26 09:03:47 +0800

    整理1

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerControls controls;
+    private CharacterController characterController;
+    private Animator animator;
+
+    [Header("移動參數")]
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    private float speed;
+    private Vector3 movementDirection;
+    [SerializeField] private float gravityScale = 9.81f;
+    private float verticalVelocity;
+    private bool isRunning;
+
+    [Header("Aim info")]
+    [SerializeField] private Transform aim;
+    [SerializeField] private LayerMask aimLayerMask;
+    private Vector3 lookingDirection;
+
+    private Vector2 moveInput;
+    private Vector2 aimInput;
+
+    private void Awake()
+    {
+        //定義新版控制器事件
+        AssignInputEvents();
+    }
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+
+        speed = walkSpeed;
+    }
+
+    private void Update()
+    {
+        //移動
+        ApplyMovement();
+        //瞄準面相準星
+        AimTowardMouse();
+        //動畫
+        AnimatorControllers();
+
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("射擊");
+        animator.SetTrigger("Fire");
+    }
+
+    private void AnimatorControllers()
+    {
+        float xVelocity = Vector3.Dot(movementDirection.normalized, transform.right);
+        float zVelocity = Vector3.Dot(movementDirection.normalized, transform.forward);
+
+        //第三,四個參數是線性插值
+        animator.SetFloat("xVelocity", xVelocity, .1f, Time.deltaTime);
+        animator.SetFloat("zVelocity", zVelocity, .1f, Time.deltaTime);
+
+        bool playRunAnimation = isRunning && movementDirection.magnitude > 0;
+        animator.SetBool("isRunning", playRunAnimation);
+    }
+
+    private void AimTowardMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(aimInput);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, aimLayerMask))
+        {
+            lookingDirection = hitInfo.point - transform.position;
+            lookingDirection.y = 0f;
+            lookingDirection.Normalize();
+
+            transform.forward = lookingDirection;
+
+            aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        movementDirection = new Vector3(moveInput.x, 0, moveInput.y);
+
+        //重力
+        ApplyGravity();
+
+        if (movementDirection.magnitude > 0)
+        {
+            characterController.Move(movementDirection * Time.deltaTime * speed);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (characterController.isGrounded == false)
+        {
+            //Edit的projectSetting那邊有個physics/Gravity設定,預設為9.81所以這邊用9.81
+            verticalVelocity -= gravityScale * Time.deltaTime;
+            movementDirection.y = verticalVelocity;
+        }
+        else
+        {
+            verticalVelocity = -0.5f;
+        }
+    }
+
+    #region 新版輸入系統
+    private void AssignInputEvents()
+    {
+        controls = new PlayerControls();
+
+        controls.Charcater.Fire.performed += context => Shoot();
+
+        controls.Charcater.Movement.performed += context => moveInput = context.ReadValue<Vector2>();
+        controls.Charcater.Movement.canceled += context => moveInput = Vector2.zero;
+        controls.Charcater.Aim.performed += context => aimInput = context.ReadValue<Vector2>();
+        controls.Charcater.Aim.canceled += context => aimInput = Vector2.zero;
+
+        controls.Charcater.Run.performed += context =>
+        {
+            speed = runSpeed;
+            isRunning = true;
+        };
+
+        controls.Charcater.Run.canceled += context =>
+        {
+            speed = walkSpeed;
+            isRunning = false;
+
+        };
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    #endregion
+}
