7db3260e51d0db685e8ab284fb55e0f3590bcb67
willy <willyjen1111@gmail.com>
2024-12-27 19:43:23 +0800

    [42課] 設定子彈瞄準方向

--- Assets/Scripts/PlayerWeaponController.cs
+++ Assets/Scripts/PlayerWeaponController.cs
@@ -11,6 +11,10 @@ public class PlayerWeaponController : MonoBehaviour
     [SerializeField] private float bulletSpeed;
     [SerializeField] private Transform gunPoint;
 
+    [SerializeField] private Transform weaponHolder;
+    [SerializeField] private Transform aim;
+
+
     private void Start()
     {
         player = GetComponent<Player>();
@@ -21,10 +25,32 @@ public class PlayerWeaponController : MonoBehaviour
     {
         GameObject newBullet = Instantiate(bulletPrefab, gunPoint.position, Quaternion.LookRotation(gunPoint.forward));
 
-        newBullet.GetComponent<Rigidbody>().velocity = gunPoint.forward * bulletSpeed;
+        newBullet.GetComponent<Rigidbody>().velocity = BulletDirection() * bulletSpeed;
 
         Destroy(newBullet, 10f);
 
         GetComponentInChildren<Animator>().SetTrigger("Fire");
+    }
+
+    //固定住y軸
+    private Vector3 BulletDirection()
+    {
+        //將槍口及子彈生成時的瞄準方向瞄準到aim上
+        weaponHolder.LookAt(aim);
+        gunPoint.LookAt(aim);
+
+        Vector3 direction = (aim.position - gunPoint.position).normalized;
+        direction.y = 0;
+
+        return direction;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(weaponHolder.position, weaponHolder.position + weaponHolder.forward * 25f);
+
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawLine(gunPoint.position, gunPoint.position + BulletDirection() * 25f);
     }
 }
