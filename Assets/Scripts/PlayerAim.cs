6e3fb56f486df0954cca52dedae4ba091111d1d2
willy <willyjen1111@gmail.com>
2024-12-27 16:29:46 +0800

    [39課] 攝影機看相前面

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -8,9 +8,18 @@ public class PlayerAim : MonoBehaviour
     private PlayerControls controls;
 
     [Header("Aim info")]
+    [Range(0.5f, 1f)]
+    [SerializeField] private float minCamaraDistance = 1.5f;
+    [Range(1f, 3f)]
+    [SerializeField] private float maxCamaraDistance = 4f;
+
+    [Range(3f, 5f)]
+    [SerializeField] private float aimSensetivity = 5f;
+
+
     [SerializeField] private Transform aim;
     [SerializeField] private LayerMask aimLayerMask;
-    private Vector3 lookingDirection;
+
     private Vector2 aimInput;
 
     private void Start()
@@ -22,7 +31,29 @@ public class PlayerAim : MonoBehaviour
 
     private void Update()
     {
-        aim.position = new Vector3(GetMosuePosition().x, transform.position.y + 1, GetMosuePosition().z);
+        aim.position = Vector3.Lerp(aim.position, DesieredAimPosition(), Time.deltaTime * aimSensetivity);
+    }
+
+    //理想的準心位置
+    private Vector3 DesieredAimPosition()
+    {
+
+        //透過actualMaxCamaraDistance防止玩家往下跑時玩家超出攝影機
+        float actualMaxCamaraDistance = player.movement.moveInput.y < -0.5f ? minCamaraDistance : maxCamaraDistance;
+
+
+        //計算攝影機準心位置
+        Vector3 desiredAimPosition = GetMosuePosition();
+        Vector3 aimDirection = (desiredAimPosition - transform.position).normalized;
+
+        float distanceToDesiredPosition = Vector3.Distance(transform.position, desiredAimPosition);
+        float clampedDistance = Mathf.Clamp(distanceToDesiredPosition, minCamaraDistance, actualMaxCamaraDistance);
+
+        desiredAimPosition = transform.position + aimDirection * clampedDistance;
+
+        desiredAimPosition.y = transform.position.y + 1;
+
+        return desiredAimPosition;
     }
 
     public Vector3 GetMosuePosition()
