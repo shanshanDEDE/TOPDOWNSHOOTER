3e40227a6a0c47b473930d6676a6bf5e133b38f7
willy <willyjen1111@gmail.com>
2025-01-02 15:03:58 +0800

    [89課] AI Navigation Setup

---
+++ Assets/ExampleEnemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ExampleEnemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform target;
+
+    private void Update()
+    {
+        agent.destination = target.position;
+    }
+}
